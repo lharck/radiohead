9680a7f236def0a0e24b6edd2204c2ccf25012b7
Lucy Harck <32280206+lharck@users.noreply.github.com>
2019-10-21 17:03:01 -0500

    Add files via upload

---
+++ lharck.github.io-4157cfa6dd0e5edbf010d72005289917dfa47eb1/audioPlayer.js
@@ -0,0 +1,126 @@
+
+var dictonary = {
+	"Packt Like Sardines in a Crushed Tin Box": "https://open.spotify.com/embed/track/7he52OqF36BVF6dSVKKQPq",
+	"I Might Be Wrong":"https://open.spotify.com/embed/track/0JhpYE9xA3UpSFrmXTZyIF",
+	"Dollars and Cents":"https://open.spotify.com/embed/track/2mvmuCtmmrERnm03ULdDye",
+	"Pyrimad Song":"https://open.spotify.com/embed/track/3A9vIxzGBjEfqmDK7H9exS",
+	"Knives Out":"https://open.spotify.com/embed/track/2mvmuCtmmrERnm03ULdDye",
+	"Hunting Bears":"https://open.spotify.com/embed/track/2zYrFer4QGSQkk5aUawfHB",
+	"Pulk/Pull Revolving Doors":"https://open.spotify.com/embed/track/4doFWLhPChHDgs89rHs5nr",
+	"Morning Bell/Amnesiac":"https://open.spotify.com/embed/track/3jaA0iP323UuDVVDA9sfSl",
+	"Like Spinning Plates":"https://open.spotify.com/embed/track/1qVP3GP1pUhJxogteiIi78",
+	"You and Whose Army?":"https://open.spotify.com/embed/track/6UFvGJalSngqprmUJd59Eo",
+	"Life In a Glasshouse":"https://open.spotify.com/embed/track/29yXMFKIrY1fFY4DYBVZCu",
+	"Everything In Its Right Place":"https://open.spotify.com/embed/track/5AiNZnMDCWwujIENPj9PV9",
+	"Kid A":"https://open.spotify.com/embed/track/4x7XJOE2ml2P3o3i77466j",
+	"The National Anthem":"https://open.spotify.com/embed/track/01UstUPbzPgq2MNp6h0tVo",
+	"Treefingers":"https://open.spotify.com/embed/track/4DPQvbgSM0IdX4O3HOACwL",
+	"Optimistic":"https://open.spotify.com/embed/track/4DE6Wkt9fW7R5e9gJGWQim",
+	"In Limbo":"https://open.spotify.com/embed/track/1NgvIjkEjkhcIR1pp5Qsir",
+	"Idioteque":"https://open.spotify.com/embed/track/7xqeIdLJSf3bgmZ7vUvHrE",
+	"Morning Bell":"https://open.spotify.com/embed/track/1HQYo8C5L0qd1p5f1anXPS",
+	"Motion Picture Soundtrack":"https://open.spotify.com/embed/track/0COiZ7ncho2yi4HotbzgPv",
+	"Untitled":"https://open.spotify.com/embed/track/2YsJM80gRMAhCikUkNZT3G",
+
+	"How to Disappear Completely":"https://open.spotify.com/embed/track/69pwmeyvQMuHMtkCmpEWhQ",
+	
+	"Planet Telex":"https://open.spotify.com/embed/track/05evePUsIT1cmIURp1hgu6",
+	"The Bends":"https://open.spotify.com/embed/track/3aDUSpF3LexOr1lFKvPV2h",
+	"High And Dry":"https://open.spotify.com/embed/track/5jafMI8FLibnjkYTZ33m0c",
+	"Fake Plastic Trees":"https://open.spotify.com/embed/track/045sp2JToyTaaKyXkGejPy",
+	"Bones":"https://open.spotify.com/embed/track/0OT4Rhd9cw1yajN9ZxL6qL",
+	"(Nice Dream)":"https://open.spotify.com/embed/track/4QbQ55kHcRNho6XLaPctT1",
+	"Just":"https://open.spotify.com/embed/track/4SE81CrzH0qPA8KHqM9Syz",
+	"My Iron Lung":"https://open.spotify.com/embed/track/4dPKQxaraW6CG1rTBzV6DW",
+	"Bullet Proof (I Wish I was)":"",
+	"Black Star":"https://open.spotify.com/embed/track/6JzzI3YxHCcjZ7MCQS2YS1",
+	"Sulk":"https://open.spotify.com/embed/track/4HtPAkZnSyGtNvBnfDc2nw",
+	"Street Spirit (Fade Out)":"https://open.spotify.com/embed/track/5gz38AxRkD6Ywxd4fr2pLj",
+	"Airbag":"https://open.spotify.com/embed/track/3OsUjkcv1C1v5udFhgRSFg",
+	"Paranoid Android":"https://open.spotify.com/embed/track/2nTsKOXIVGDf2iPeVQO2Gm",
+	"Subterranean Homesick Alien":"https://open.spotify.com/embed/track/19yGmm9FjEZdZc5j98WDe4",
+	"Exit Music (For a Film)":"https://open.spotify.com/embed/track/4Na0siMtWOW9pJoWJ1Ponv",
+	"Let Down":"https://open.spotify.com/embed/track/4aOAzvRdOsZSwZIgwcdeL0",
+	"Karma Police":"https://open.spotify.com/embed/track/3SVAN3BRByDmHOhKyIDxfC",
+	"Fitter Happier":"https://open.spotify.com/embed/track/53aFGAl0Vebwp2bo8wtdWR",
+	"Electioneering":"https://open.spotify.com/embed/track/6PBHfI3G8MlQ8qdItoVoxL",
+	"Climbing Up the Walls":"https://open.spotify.com/embed/track/0R332cdlS9LyE0Ge2PSlVC",
+	"No Suprises":"https://open.spotify.com/embed/track/1bSpwPhAxZwlR2enJJsv7U",
+	"Lucky":"https://open.spotify.com/embed/track/6n7GUf2h8D2Ad2wUy5s7nE",
+	"The Tourist":"https://open.spotify.com/embed/track/6n7GUf2h8D2Ad2wUy5s7nE",
+
+	"Bloom":"https://open.spotify.com/embed/track/6tLALP9mi7VbNC7s16iBRK",
+	"Morning Mr Magpie":"https://open.spotify.com/embed/track/3wxmJSQ3Z1HfAuxeHQoZcE",
+	"Little By Little":"https://open.spotify.com/embed/track/4tCwsdqqG4jASHhbsMHCx0",
+	"Feral":"https://open.spotify.com/embed/track/0eECFDnWy0RdjMmJ8NOeAL",
+	"Lotus Flower":"https://open.spotify.com/embed/track/1amSa5xo79zINsgrpNlNge",
+	"Codex":"https://open.spotify.com/track/embed/4sLGaMl33I6mcsk7u4xHAU",
+	"Give Up The Ghost":"https://open.spotify.com/embed/track/4OrGm3fBdhlXuHtRBoLUMW",
+	"Separator":"https://open.spotify.com/embed/track/2rA36OZNb3LkvqcNro1ugK",
+
+	"15 Step":"https://open.spotify.com/embed/track/6dsq7Nt5mIFzvm5kIYNORy",
+	"Bodysnatchers":"https://open.spotify.com/embed/track/4m0Vgr48VFaMYw0Sp1ozJu",
+	"Nude":"https://open.spotify.com/embed/track/5k7VKj1Xwy5DjO4B0PdAOb",
+	"Weird Fishes/ Arpeggi":"https://open.spotify.com/embed/track/4Iyo50UoYhuuYORMLrGDci",
+	"All I Need":"https://open.spotify.com/embed/track/51ygW389BW4Dut3B69pSwc",
+	"Faust Arp":"https://open.spotify.com/embed/track/5SdmtFbNOD7Qej6jFCHkOM",
+	"Reckoner":"https://open.spotify.com/embed/track/56Z7hbyMrndw1naxb6I5Oi",
+	"House Of Cards":"https://open.spotify.com/embed/track/3Jc6X15OZCCyhGSHBF4hwB",
+	"Jigsaw Falling Into Place":"https://open.spotify.com/embed/track/15ea10YpJIl3mJq75yzqsD",
+	"Videotape":"https://open.spotify.com/embed/track/3uxhyRdWVXp7GQvERQl6fA",
+
+	"2 + 2 = 5":"https://open.spotify.com/embed/track/37kUGdEJJ7NaMl5LFW4EA4",
+	"Sit Down. Stand Up.":"https://open.spotify.com/embed/track/3wPLVTqp66miOokxN1cLbm",
+	"Sail To The Moon":"https://open.spotify.com/embed/track/5olWalQH1oVza5D6xJg4oX",
+	"Backdrifts":"https://open.spotify.com/embed/track/6CVdTDYoDbwYj4xn8u5Gha",
+	"Go To Sleep":"https://open.spotify.com/embed/track/6TdGeb9e2KL6RZG0VxoTMd",
+	"Where I End and You Begin":"https://open.spotify.com/embed/track/5X3uhutgZktlUWmzIOE3NG",
+	"We Suck Young Blood":"https://open.spotify.com/embed/track/53RYTfkLiyZuUZHwJC3Wmm",
+	"The Gloaming":"https://open.spotify.com/embed/track/2q4zYomFf4ZVrEOr1aXj6l",
+	"There, There":"https://open.spotify.com/embed/track/77SPrxY5gn6VPrKPGrfLnM",
+	"I Will":"https://open.spotify.com/embed/track/28VoI90xDBoKe5sP3sP5jW",
+	"A Punch Up At a Wedding":"https://open.spotify.com/embed/track/2LfUYXF8jfrHCfwYyf2pRj",
+	"Myxomatosis":"https://open.spotify.com/embed/track/38WL6GlG9NHFiQS6JxV84r", 
+	"Scatterbrain":"https://open.spotify.com/embed/track/467FBnBDRfL8Hz9ktUUybj",
+	"A Wolf at the Door":"https://open.spotify.com/embed/track/66cuLvkHsW7dNkfNk6gQP9",
+
+	"Burn The Witch":"https://open.spotify.com/embed/track/3pcCifdPTc2BbqmWpEhtUd",
+	"Daydreaming":"https://open.spotify.com/embed/track/1uRxyAup7OYrlh2SHJb80N",
+	"Decks Dark":"https://open.spotify.com/embed/track/5rIhBK9aaVMck0W2YtOwci",
+	"Desert Island Disk":"https://open.spotify.com/embed/track/1kBGeOp1CDUHVdbK4ergqo",
+	"Ful Stop":"https://open.spotify.com/embed/track/4CzTgOmc3Sdm4EgKQWzjQl",
+	"Glass Eyes":"https://open.spotify.com/embed/track/0eZN5WsQfmNFICHuw59Zfz",
+	"Identikit":"https://open.spotify.com/embed/track/3LhtqibvTtjOUrzKs7Vsz1",
+	"The Numbers":"https://open.spotify.com/embed/track/6f6pEjgfTtuRROmJ4a7Gf3",
+	"Present Tense":"https://open.spotify.com/embed/track/4eruRiSfDY1jdT03hjyi0i",
+	"Tinker Tailor Soilder":"https://open.spotify.com/embed/track/3cual6JOG286qZJmCxKRAT",
+	"True Love Waits":"https://open.spotify.com/embed/track/01ZpFhrMMqKPVCwyqXneVp",
+
+	"You":"https://open.spotify.com/embed/track/1MyqLTRhgyWPw7v107BEuI",
+	"Creep":"https://open.spotify.com/embed/track/6b2oQwSGFkzsMtQruIWm2p",
+	"How Do You Do?":"https://open.spotify.com/embed/track/71wIOoaoVMUwskK5yCXZL4",
+	"Stop Whispering":"https://open.spotify.com/embed/track/2zYmvi3w2T8a9Ckrv21bvW",
+	"Thinking About You":"https://open.spotify.com/embed/track/3ovbSnT5NNhl1gzMcw1NRZ",
+	"Anyone Can Play Guitar":"https://open.spotify.com/embed/track/4NUc1M0CS7b6zvWoyvibju",
+	"Ripcord":"https://open.spotify.com/embed/track/4nklmaiY4gfQI3SB1sLGsd",
+	"Vegtable":"https://open.spotify.com/embed/track/11qDTSr3Dj4TkPnBcIOqEJ",
+	"Prove Yourself":"https://open.spotify.com/embed/track/4D6ZExVvYLZxhcAvifX5px",
+	"I Can't":"https://open.spotify.com/embed/track/48IEDejXX5LH8TAC3VIGpc",
+	"Lurgee":"https://open.spotify.com/embed/track/2B5P22cfUadACK7jLQegU6",
+	"Blow Out":"https://open.spotify.com/embed/track/6qttbImnJ5wuA8AtDKEy18"
+};
+
+
+function playSong(tag) {
+	var songName = tag.getElementsByTagName('a')[0].textContent;
+	console.log(songName);
+	if(dictonary[songName] != null)
+	{
+	  	document.getElementById("musicPlayer").src = dictonary[songName];
+	 }
+	 else
+	{
+		console.log("[ERROR]: Song not in dictonary");
+	}
+}
+
